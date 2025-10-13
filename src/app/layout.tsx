01c2a9d5117cffdb07e675d81e6d4b048f16d721
fable <pandeyshivanshu2005@gmail.com>
2025-10-13 20:51:59 +0530

    relevant changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 /* just figure out the  design */
-/*need to find the params of the related topic */
+/*need to find the params of the mechanical components */
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
